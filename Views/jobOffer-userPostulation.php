20cc1ec78ac2d47c103a7b926a1d53fb5a7469cf
Mar <orpianesi.mar@gmail.com>
2021-11-07 14:31:08 -0300

    Add DropOfferUser, controller, view and send email

---
+++ Views/jobOffer-userPostulation.php
@@ -0,0 +1,28 @@
+<?php
+         if (!isset($_SESSION['loggedUser']))
+         require_once('logIn.php');
+       else {
+          if(substr($_SESSION['loggedUser']->getId(),0,2) == "AD")
+             require_once('admin-nav.php');
+    
+?>
+<main class="py-5">
+    <section id="listado" class="mb-5">
+        <div class="container">
+            <h2 class="mb-4">Postulaciones activas "<?php echo $tittle?>"?</h2>
+            <form action="<?php echo FRONT_ROOT ?>JobOffer/DropOfferUser" method="get" class="bg-light-alpha p-5">
+                        <table class="table bg-light-alpha">
+                <?php foreach ($postulationDAO->GetAllByOffer($offerID) as $user) {?>
+                    <tbody>
+                    <tr>
+                        <td><strong>Usuario Postulado:</strong> <?php echo $user->getUserName() ?></td>
+                    <tr>
+                    <?php echo "<a href='".FRONT_ROOT."JobOffer/ShowDropOfferUserView/".$user->getUserName()."/".$jobOffer->getOfferID()."'> Dar de baja </a>";?>
+                           
+                <?php } ?>
+
+            </form>
+        </div>
+    </section>
+</main>
+<?php } ?>
