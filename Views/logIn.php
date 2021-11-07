4b2e4a9bf151df3b4a0e6d223c730d80421e7a28
Nahuel Tolosa <josetolosa98@gmail.com>
2021-11-06 23:02:31 -0300

    log in style

--- Views/logIn.php
+++ Views/logIn.php
@@ -1,22 +1,23 @@
-<div>
-    <form action="<?php echo FRONT_ROOT ?>LogIn/ValidateLogIn" method="post" class="bg-light-alpha p-5">
-        <?php if(isset($message)) echo $message?>
-        <div class="col-lg-4" style= "width:85%; margin: auto;">
-            <div class="form-group">
+<div class="main-container">
+    <form action="<?php echo FRONT_ROOT ?>LogIn/ValidateLogIn" method="post" class="container log-in bg-light-alpha">
+        <div class="log-in__message">
+            <?php if(isset($message)) echo $message?>
+        </div>
+        <div class="log-in__input">
+            <div class="input-area">
                 <label for="logInMail"><strong>Ingrese su usuario</strong></label>
-                <input type="mail" name="username" value="" class="form-control">
-                <label for="password"><strong>Ingrese su contraseña</strong></label>
-                <input type="password" name="password" value="" class="form-control">
-                <br>
-                <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Ingresar</button>
-                <br>
-                <button type="submit" name="button" class="btn btn-dark ml-auto d-block">
-                    <a href="<?php echo FRONT_ROOT."User/ShowSignInView" ?>" style="text-decoration: none;color: #fff;">Sign In</a>
-                </button>
+                <input type="mail" name="username" value="" class="input">
             </div>
-            
-            
+            <div class="input-area">
+                <label for="password"><strong>Ingrese su contraseña</strong></label>
+                <input type="password" name="password" value="" class="input">
+            </div>
+            <div class="input-area--buttons">
+                <button type="button" name="button" class="button">
+                    <a href="<?php echo FRONT_ROOT."User/ShowSignInView" ?>">Sign In</a>
+                </button>
+                <button type="submit" name="button" class="button">Ingresar</button>
+            </div>
         </div>
     </form>
 </div>
-<!-- ddouthwaite0@goo.gl -->
