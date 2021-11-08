557bc6f955281b539600b35647f6a25b2a18a60a
Nahuel Tolosa <josetolosa98@gmail.com>
2021-11-08 01:30:50 -0300

    solucion de incompatibilidades con la clase joboffer

--- Models/JobOffer.php
+++ Models/JobOffer.php
@@ -3,98 +3,50 @@
 
     class JobOffer
     {
+        private $offerID;
+        private $tittle;
         private $idCompany;
-        private $creationDate;
+        private $date;
         private $description;
         private $salary;
+        private $workDay;
         private $active;
+        private $reference;
         private $postulations= array();
 
-        /**
-         * Get the value of idCompany
-         */ 
-        public function getIdCompany()
+        public function __construct()
         {
-                return $this->idCompany;
+                $this->active=true;
+                $this->date=date("d-m-Y");
+                $this->offerID=$this->generateID();
+        }
+
+        private function generateID(){
+                return "JO".rand(100000,999999).chr(rand(ord('A'),ord('Z')));
         }
 
         /**
-         * Set the value of idCompany
-         *
-         * @return  self
-         */ 
-        public function setIdCompany($idCompany)
+         * Get the value of postulations
+         */
+        public function getPostulations()
         {
-                $this->idCompany = $idCompany;
+                return $this->postulations;
+        }
+
+        /**
+         * Set the value of postulations
+         */
+        public function setPostulations($postulations): self
+        {
+                $this->postulations = $postulations;
 
                 return $this;
         }
-
-        /**
-         * Get the value of creationDate
-         */ 
-        public function getCreationDate()
-        {
-                return $this->creationDate;
-        }
-
-        /**
-         * Set the value of creationDate
-         *
-         * @return  self
-         */ 
-        public function setCreationDate($creationDate)
-        {
-                $this->creationDate = $creationDate;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of description
-         */ 
-        public function getDescription()
-        {
-                return $this->description;
-        }
-
-        /**
-         * Set the value of description
-         *
-         * @return  self
-         */ 
-        public function setDescription($description)
-        {
-                $this->description = $description;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of salary
-         */ 
-        public function getSalary()
-        {
-                return $this->salary;
-        }
-
-        /**
-         * Set the value of salary
-         *
-         * @return  self
-         */ 
-        public function setSalary($salary)
-        {
-                $this->salary = $salary;
-
-                return $this;
-        }
-
 
         /**
          * Get the value of active
          */
-        public function isActive()
+        public function getActive()
         {
                 return $this->active;
         }
@@ -110,25 +62,158 @@
         }
 
         /**
-         * Get the value of postulations
-         */ 
-        public function getPostulations()
+         * Get the value of salary
+         */
+        public function getSalary()
         {
-                return $this->postulations;
+                return $this->salary;
         }
 
         /**
-         * Set the value of postulations
+         * Set the value of salary
+         */
+        public function setSalary($salary): self
+        {
+                $this->salary = $salary;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of date
+         */
+        public function getDate()
+        {
+                return $this->date;
+        }
+
+        /**
+         * Set the value of date
+         */
+        public function setDate($date): self
+        {
+                $this->date = $date;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of idCompany
+         */
+        public function getIdCompany()
+        {
+                return $this->idCompany;
+        }
+
+        /**
+         * Set the value of idCompany
+         */
+        public function setIdCompany($idCompany): self
+        {
+                $this->idCompany = $idCompany;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of description
+         */
+        public function getDescription()
+        {
+                return $this->description;
+        }
+
+        /**
+         * Set the value of description
+         */
+        public function setDescription($description): self
+        {
+                $this->description = $description;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of tittle
+         */
+        public function getTittle()
+        {
+                return $this->tittle;
+        }
+
+        /**
+         * Set the value of tittle
+         */
+        public function setTittle($tittle): self
+        {
+                $this->tittle = $tittle;
+
+                return $this;
+        }
+
+        public function getCompanyById(){
+                $companyController = new CompanyController();
+    
+                return $companyController->getCompanyById($this->idCompany);
+            }
+
+        /**
+         * Get the value of offerID
+         */
+        public function getOfferID()
+        {
+                return $this->offerID;
+        }
+
+        /**
+         * Set the value of offerID
+         */
+        public function setOfferID($offerID): self
+        {
+                $this->offerID = $offerID;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of workDay
+         */
+        public function getWorkDay()
+        {
+                return $this->workDay;
+        }
+
+        /**
+         * Set the value of workDay
+         */
+        public function setWorkDay($workDay): self
+        {
+                $this->workDay = $workDay;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of reference
+         */ 
+        public function getReference()
+        {
+                return $this->reference;
+        }
+
+        /**
+         * Set the value of reference
          *
          * @return  self
          */ 
-        public function setPostulations($postulations)
+        public function setReference($reference)
         {
-                $this->postulations = $postulations;
+                $this->reference = $reference;
 
                 return $this;
         }
     }
+
     
 
 ?>
